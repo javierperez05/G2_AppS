d2edb5f82166d76ef62638eb506a259d3571c03e
Unai Caño <unai.canomu@elorrieta-errekamari.com>
2026-03-11 19:18:37 +0100

    fragments iniciados

--- app/src/main/java/com/example/cosmos/CosmosApp.kt
+++ app/src/main/java/com/example/cosmos/CosmosApp.kt
@@ -1,10 +1,12 @@
 package com.example.cosmos
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.cosmos.ui.HUD.NavigationHUD
 
 class CosmosApp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,12 @@ class CosmosApp : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+        //intent para abrir el HUD
+        val intent = Intent(this, NavigationHUD::class.java)
+        startActivity(intent)
+
+
     }
 }
--- app/src/main/java/com/example/cosmos/ui/HUD/NavigationHUD.kt
+++ app/src/main/java/com/example/cosmos/ui/HUD/NavigationHUD.kt
@@ -5,9 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.cosmos.R
 
 class NavigationHUD : AppCompatActivity() {
+
+    private lateinit var binding: com.example.cosmos.databinding.ActivityNavigationHudBinding
+    private lateinit var navController: NavController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +25,20 @@ class NavigationHUD : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
+
+
+    private fun initUI() {
+        initNavigation()
+
+    }
+
+    private fun initNavigation() {
+        val navHost: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomMenu.setupWithNavController(navController)
+    }
+
 }
